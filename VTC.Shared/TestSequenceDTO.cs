44031d416b0ba9423e24a1f06d5cb2ffa9d15d76
Gui <guilhermenuno17@gmail.com>
2025-03-13 12:46:19 +0000

    Added shared component to share classes. Added TestSequence Object

---
+++ VTC.Shared/TestSequenceDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace VTC.Shared;
+
+public class TestSequenceDTO
+{
+    public int SetPoint { get; set; }
+    public int Duration { get; set; }
+
+    public TestSequenceDTO(int setPoint, int duration)
+    {
+        SetPoint = setPoint;
+        Duration = duration;
+    }
+}
